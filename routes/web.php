ccec8f5f0c74f6ec67f339802102b1b4f2f4b8a2
Jerdon07 <jdlitaoen@gmail.com>
2026-01-31 17:05:29 +0800

    REFACTOR: Change the home route to portfolio controller

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
+use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+Route::get('/', [PortfolioController::class, 'index'])->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
